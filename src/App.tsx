30a8667db72cdb29ae14994e00445e92db3f5267
Winnie-Udel <winnie@udel.edu>
2025-03-05 14:56:22 -0500

    Completed all tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import "./App.css";
+import { Counter } from "./components/Counter";
+import { RevealAnswer } from "./components/RevealAnswer";
+import { ChangeType } from "./components/ChangeType";
+import { StartAttempt } from "./components/StartAttempt";
+import { TwoDice } from "./components/TwoDice";
+import { CycleHoliday } from "./components/CycleHoliday";
 
 function App(): React.JSX.Element {
     return (
@@ -11,6 +17,29 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <hr></hr>
+            <Counter></Counter>
+            <hr />
+
+            <hr></hr>
+            <RevealAnswer></RevealAnswer>
+            <hr />
+
+            <hr></hr>
+            <ChangeType></ChangeType>
+            <hr />
+
+            <hr></hr>
+            <StartAttempt></StartAttempt>
+            <hr />
+
+            <hr></hr>
+            <TwoDice></TwoDice>
+            <hr />
+
+            <hr></hr>
+            <CycleHoliday></CycleHoliday>
+            <hr />
         </div>
     );
 }
